12b717098ea222beca16e3450388d88ea55c3eee
Alexander Hussey <alex@HUSSDOGG.com>
2022-08-31 17:55:19 +1000

    Alex - initial devfile

---
+++ hello-world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+A simple Python application that prints 'Hello World'
+"""
+
+def main():
+    """
+    main program loop
+    """
+    print("Hello, world!")
+
+if __name__ == '__main__':
+    main()
